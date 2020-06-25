8c78ae01d7176577d57287552008155a1fb3b218
Rares Octavian <myonlybaraoc@gmail.com>
2020-06-25 17:29:16 +0300

    update 2

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -6,7 +6,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\Tests\ProductRepositoryClass;
 
 class PageController extends AbstractController
 {
@@ -14,7 +13,6 @@ class PageController extends AbstractController
      * @Route("/")
      */
     public function mainPage() {
-        $test = new ProductRepositoryClass();
 
         return new Response('Hello World!');
     }
